78d9b7d1852a397d566a5f4d7762ff8db0b3fcc4
TatianaMaksakova <tatzianar@gmail.com>
2023-12-07 15:23:32 +0300

    add change

--- myprojectaaa/blog/views.py
+++ myprojectaaa/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return HttpResponse("<h1>Привет</h1>")
